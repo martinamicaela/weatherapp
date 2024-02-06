fae8ee0acdc651b60ae4db882c14c47b01fe45df
martinamicaela frutos <martinamicaela924@gmail.com>
2024-02-06 22:12:34 +0000

    fix API URL remove error typo

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -6,7 +6,7 @@ const API_KEY="2e8632aaf4cafb36fca28129a00dbda7"; //API.KEY.for OpenWeatherMap A
 const getCityCoordinates=() => {
     const cityName = cityInput.value.trim(); // Get user entered city name and remove extra spaces
     if(!cityName) return;//Return if cityName is empty
-    const GEOCODING_API_URL= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}London&limit=1&appid=${API_KEY}`;
+    const GEOCODING_API_URL= `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         console.log(data)
